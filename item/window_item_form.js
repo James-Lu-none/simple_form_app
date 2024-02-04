e179b3bd7165da9d8ac96b1a437b6c2fb772960e
James-Lu-none <jamesabcde277@gmail.com>
2024-02-04 23:25:08 +0800

    add addWindowItem function

--- item/window_item_form.js
+++ item/window_item_form.js
@@ -13,3 +13,24 @@ function addItem() {
     updateItemList();
     updateTotal();
 }
+
+function addWindowItem() {
+    const item = document.getElementById('item').value;
+    const windowLength = document.getElementById("windowLength").value;
+    const windowWidth =document.getElementById("windowWidth").value;
+    const pattern = windowLength + "x" + windowWidth + "cm2"; 
+    const quantity = document.getElementById('quantity').value;
+    
+    
+    const unitPrice = document.getElementById('unitPrice').value;
+
+    const ft3Value =windowLength*windowWidth/900;
+
+    const unit = document.getElementById('unit').value;
+    
+    const total =  Math.round(quantity * unitPrice * ft3Value);
+    items.push({ item, pattern,quantity, unitPrice, unit, total});
+
+    updateItemList();
+    updateTotal();
+}
