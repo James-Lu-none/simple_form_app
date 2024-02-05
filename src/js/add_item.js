bdf10e3c946046082a735531f0e67cb24ac4bd75
James-Lu-none <jamesabcde277@gmail.com>
2024-02-06 00:13:40 +0800

    fix add un_categorized item

--- src/js/add_item.js
+++ src/js/add_item.js
@@ -1,11 +1,10 @@
 function addItem() {
-    const item = document.getElementById('item').value;
-    const pattern = '';
-    const quantity = document.getElementById('quantity').value;
-    
-    const unitPrice = document.getElementById('unitPrice').value;
-    const unit = document.getElementById('unit').value;
-    
+    const item = document.getElementById('u_item').value;
+    const pattern = document.getElementById('u_pattern').value;
+    const quantity = document.getElementById('u_quantity').value;
+    const unitPrice = document.getElementById('u_unitPrice').value;
+    const unit = document.getElementById('u_unit').value;
+    if(!item | !quantity | !unitPrice) return;
     const total = quantity * unitPrice;
     items.push({ item, pattern,quantity, unitPrice, unit, total});
     
