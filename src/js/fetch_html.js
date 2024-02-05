a6a48f822e2ba7fe7dd542d741ee8469181743aa
James-Lu-none <jamesabcde277@gmail.com>
2024-02-06 01:02:00 +0800

    fix unable to get unit selector by id

--- src/js/fetch_html.js
+++ src/js/fetch_html.js
@@ -1,18 +1,22 @@
 (function() {
     
-    const windowItemFormContainer = document.getElementById('windowItemFormContainer');
-    fetch('src/html/item/window_item_form.html')
-    .then(response => response.text())
-    .then(html => {
-      windowItemFormContainer.innerHTML = html.trim();
-    })
-    .catch(error => console.error('Error loading window_item_form.html:', error));
+  const windowItemFormContainer = document.getElementById('windowItemFormContainer');
+  fetch('src/html/item/window_item_form.html')
+  .then(response => response.text())
+  .then(html => {
+    windowItemFormContainer.innerHTML = html.trim();
+    const windowItemUnit=document.getElementById('windowUnit');
+    setUnitOptions(windowItemUnit);
+  })
+  .catch(error => console.error('Error loading window_item_form.html:', error));
 
-    const unknownItemFormContainer = document.getElementById('unknownItemFormContainer');
-    fetch('src/html/item/unknown_item_form.html')
-    .then(response => response.text())
-    .then(html => {
-      unknownItemFormContainer.innerHTML = html.trim();
-    })
-    .catch(error => console.error('Error loading unknown_item_form.html:', error));
+  const unknownItemFormContainer = document.getElementById('unknownItemFormContainer');
+  fetch('src/html/item/unknown_item_form.html')
+  .then(response => response.text())
+  .then(html => {
+    unknownItemFormContainer.innerHTML = html.trim();
+    const unknownItemUnit=document.getElementById('u_unit');
+    setUnitOptions(unknownItemUnit);
+  })
+  .catch(error => console.error('Error loading unknown_item_form.html:', error));
 })();
--- src/js/main.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', function() {
-    const windowItemUnit=document.getElementById('windowUnit');
-    const unknownItemUnit=document.getElementById('unknownUnit');
-    setUnitOptions(windowItemUnit);
-    setUnitOptions(unknownItemUnit);
-});
