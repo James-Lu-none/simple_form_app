7669676d31f26e748360856b1a987d12cd760d25
James-Lu-none <jamesabcde277@gmail.com>
2024-02-05 17:08:45 +0800

    fix unable to drag ofter reload

--- src/js/fetch_html.js
+++ src/js/fetch_html.js
@@ -15,12 +15,4 @@
       unknownItemFormContainer.innerHTML = html.trim();
     })
     .catch(error => console.error('Error loading unknown_item_form.html:', error));
-
-    const itemTableContainer = document.getElementById('itemTableContainer');
-    fetch('src/html/item_table.html')
-    .then(response => response.text())
-    .then(html => {
-      itemTableContainer.innerHTML = html.trim();
-    })
-    .catch(error => console.error('Error loading item_table.html:', error));
 })();
