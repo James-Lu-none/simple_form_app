6da551ff10daabb005f9f55f561beffc882eb7bc
James-Lu-none <jamesabcde277@gmail.com>
2024-02-06 16:15:54 +0800

    remove unused generate from select excel

--- src/js/generate_file.js
+++ src/js/generate_file.js
@@ -1,32 +1,3 @@
-document.getElementById('select-export').addEventListener('click', () => {
-    // Assuming you have a file input for selecting the template
-    const input = document.createElement('input');
-    input.type = 'file';
-
-    input.addEventListener('change', (event) => {
-    const file = event.target.files[0];
-
-    if (file) {
-        const reader = new FileReader();
-        const cellRef = XLSX.utils.encode_cell({c: 1, r: 8});
-        reader.onload = (e) => {
-            const data = e.target.result;
-            const workbook = XLSX.read(data, { type: 'binary' });
-            
-            const sheetName = 'sheet1';
-            const worksheet = workbook.Sheets[sheetName];
-            insert_table(cellRef,worksheet)
-            // Export the modified workbook
-
-            XLSX.writeFile(workbook, 'modified_template.xlsx');
-        };
-
-        reader.readAsBinaryString(file);
-    }
-    });
-    input.click();
-});
-
 document.getElementById('default-export').addEventListener('click', () => {
     fetch('./src/template.xlsx')
     .then(response => response.arrayBuffer())
